28d2f3f9d09523c8cf9c7e3c9870b47543d1cf73
Hunter S <dook@stardust.red>
2015-05-29 04:23:44 -0400

    #8 Centralize URLs and api keys

--- cloud/keys.js
+++ cloud/keys.js
@@ -1,4 +1,22 @@
 module.exports = {
-  "mailgunDomain": "domainName",
-  "mailgunKey": "mailgunKey"
-}
+  "parse": {
+    "baseURL": "app.parseapp.com",
+    "applicationId": "appID",
+    "jsKey": "jsKey",
+    "masterKey": "masterKey"
+  },
+  "mailgun": {
+    "baseURL": "api.mailgun.net/v3/domains",
+    "domainURL": "mailgunDomainName",
+    "secretKey": "secretKey",
+    "webhooks": {
+      "bounce":       "bounceHookreceive",
+      "deliver":      "deliverHookreceive",
+      "drop":         "dropHookreceive",
+      "spam":         "spamHookreceive",
+      "unsubscribe":  "unsubscribeHookreceive",
+      "click":        "clickHookreceive",
+      "open":         "openHookreceive"
+    }
+  }
+};
