4f224f202e7181ae30310320177990ebfce72e46
Hunter S <dook@stardust.red>
2015-06-12 15:23:04 -0400

    Initial commit

---
+++ public/js/templates.js
@@ -0,0 +1,17 @@
+function getTemplate() {
+}
+
+function templateMenu(jmenu, json) {
+  // Populates div#template_menu
+  //  @jmenu: jQuery object of <ul> to populate
+  //  @json: json string or object of template objects
+}
+
+function templatePreview(jpreview, json) {
+  // Populates div#template_preview
+  //  @jmenu: jQuery object of <div> to populate
+  //  @json: json string or object of currently selected template
+}
+
+$(document).ready(function() {
+});
