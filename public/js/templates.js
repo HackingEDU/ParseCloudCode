bcb77db135132f234a0fb4b6f7fe54bbd1b89d68
Hunter S <dook@stardust.red>
2015-06-12 16:37:42 -0400

    Finished getTemplate() function

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,4 +1,33 @@
-function getTemplate() {
+function getTemplate(flimit, fpage) {
+  // Retrieves json object of all templates retrieved from server
+  //    @limit: how many templates to retrieve
+  //    @page:  page number to retrieve
+  var retval = undefined;
+
+  $.ajax(
+    {
+      url: "/action/getTemplates",
+      method: "GET",
+      data: {
+        limit: flimit,
+        page:  fpage
+      },
+      success: function(data, status) {
+        retval = data;
+        return retval;
+      },
+      error: function(jqXHR, status, err) {
+        console.log("%s: %s", status, err);
+        return retval;
+      },
+      statusCode: {
+        406: function() {
+          console.log("Server returned 'Not Acceptable'");
+          return retval;
+        }
+      }
+    }
+  );
 }
 
 function templateMenu(jmenu, json) {
