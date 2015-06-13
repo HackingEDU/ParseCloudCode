c44668cc39be291724a8a181d63a2939d82fa054
Hunter S <dook@stardust.red>
2015-06-13 03:18:40 -0400

    Refactored template organization

---
+++ public/js/template-header.js
@@ -0,0 +1,54 @@
+function getTemplates(flimit, foffset) {
+  // Retrieves json object of all templates retrieved from server
+  //    @limit:  how many templates to retrieve
+  //    @offset: item number to start retrieving from
+  // Returns a jqXHR object
+  var retval = undefined;
+
+  return $.ajax(
+    {
+      url: "/action/getTemplates",
+      method: "GET",
+      data: {
+        limit:  flimit,
+        offset: foffset
+      }
+    }
+  );
+}
+
+function templateMenu(jmenu, templates) {
+  // Populates div#template_menu and return list of jQuery objects
+  //  @jmenu: jQuery object of <ul> to populate
+  //  @templates: array of template objects
+  var promise = $.Deferred();
+  var retval = [];
+
+  try {
+    if(jmenu === undefined || jmenu === undefined) {
+      throw "Parameters undefined.";
+    }
+
+    // For each element in templates object, append to jmenu and add to retval
+    $.each(templates, function(i, value) {
+      var child = $("<li class=\"template_menu_li\">" + value.subject + "</li>");
+      child.appendTo(jmenu);
+      retval.push(child);
+    });
+
+    promise.resolve(retval);
+    return promise;
+  } catch(err) {
+    promise.reject([err]);
+    return promise;
+  }
+}
+
+function templatePreview(jpreview, json) {
+  // Populates jpreview
+  //  @jmenu: jQuery object of <div> to populate
+  //  @json: json object of currently selected template
+
+  // TODO: render editor controls
+  jpreview.html(json.bodyHTML);
+}
--- public/js/templates.js
+++ public/js/templates.js
@@ -1,57 +1,3 @@
-function getTemplates(flimit, foffset) {
-  // Retrieves json object of all templates retrieved from server
-  //    @limit:  how many templates to retrieve
-  //    @offset: item number to start retrieving from
-  // Returns a jqXHR object
-  var retval = undefined;
-
-  return $.ajax(
-    {
-      url: "/action/getTemplates",
-      method: "GET",
-      data: {
-        limit:  flimit,
-        offset: foffset
-      }
-    }
-  );
-}
-
-function templateMenu(jmenu, templates) {
-  // Populates div#template_menu and return list of jQuery objects
-  //  @jmenu: jQuery object of <ul> to populate
-  //  @templates: array of template objects
-  var promise = $.Deferred();
-  var retval = [];
-
-  try {
-    if(jmenu === undefined || jmenu === undefined) {
-      throw "Parameters undefined.";
-    }
-
-    // For each element in templates object, append to jmenu and add to retval
-    $.each(templates, function(i, value) {
-      var child = $("<li class=\"template_menu_li\">" + value.subject + "</li>");
-      child.appendTo(jmenu);
-      retval.push(child);
-    });
-
-    promise.resolve(retval);
-    return promise;
-  } catch(err) {
-    promise.reject([err]);
-    return promise;
-  }
-}
-
-function templatePreview(jpreview, json) {
-  // Populates div#template_preview
-  //  @jmenu: jQuery object of <div> to populate
-  //  @json: json object of currently selected template
-  console.log(json);
-  //jpreview.html(json.strippedHTML); // just throw it onto there... lol
-}
-
 $(document).ready(function() {
   // Template variables
   var templates = undefined;
@@ -61,13 +7,11 @@ $(document).ready(function() {
   var template_menu     = $("#template_menu");
   var template_preview  = $("#template_preview");
 
-  var pager = undefined;
   var page_limit = 10; // How many items to display
   var page_curr  = 0;  // Current page
   getTemplates(page_limit, page_curr).then(
     function success(data, status) {
       templates = data;
-      pager = new Pager(templates.length);
       return templateMenu(template_menu, templates);
     },
     function error(jqXHR, status, err) {
@@ -79,7 +23,7 @@ $(document).ready(function() {
       $.each(retval, function(i, value) {
         value.bind("click",
           function click(ev) {
-            template_preview.html(templates[i].strippedHTML);
+            templatePreview(template_preview, templates[i]);
           }
         );
       });
